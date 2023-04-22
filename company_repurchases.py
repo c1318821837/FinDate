c6d67856459aa6252363cab9fa9725e01b962b88
梁辰有点菜 <36767143+c1318821837@users.noreply.github.com>
2023-04-22 21:49:06 +0800

    Update company_repurchases.py

--- company_repurchases.py
+++ company_repurchases.py
@@ -32,20 +32,7 @@ todayjson=[]
 element=diver.find_element(By.XPATH,'/html/body/div[1]/div[8]/div[2]/div[3]/div[2]/ul/li[14]')
 element.click()
 time.sleep(2)
-address='/html/body/div[1]/div[8]/div[2]/div[3]/div[3]/div[2]/div[2]/table/tbody/tr['
-for i in range(1,50):
-    elements=diver.find_element(By.XPATH,address+str(i)+']')
-    if today==elements.find_element(By.XPATH, address + str(i) + ']' + '/td[6]').text:
-        data_dict = {
-            'StockCode': elements.find_element(By.XPATH,address+str(i)+']'+'/td[1]').text,
-            'StockName':elements.find_element(By.XPATH,address+str(i)+']'+'/td[2]').text,
-            'Title':elements.find_element(By.XPATH, address + str(i) + ']' + '/td[4]').text,
-            'Time':today,
-            'ContextUrl':elements.find_element(By.XPATH, address + str(i) + ']' + '/td[4]').find_element(By.CSS_SELECTOR,'a').get_attribute('href')
-        }
-        print(data_dict)
-        todayjson.append(data_dict)
-JsonStore(JsonUpdate(JsonOpen('repurchases.json'),todayjson))
+
 
 diver.close()
 
