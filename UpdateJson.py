3d3af6e0b0c8612d463005ce655a3507e5e96568
梁辰有点菜 <36767143+c1318821837@users.noreply.github.com>
2023-04-22 21:07:59 +0800

    Update UpdateJson.py

--- UpdateJson.py
+++ UpdateJson.py
@@ -13,5 +13,3 @@ def JsonStore(data):
     data= json.dumps(data, ensure_ascii=False)
     with open('repurchases.json', 'w', encoding='UTF-8') as f:
         f.write(data)
-
-JsonOpen('repurchases.json')
