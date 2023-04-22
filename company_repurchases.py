e18ca30eac9b3da9b1f0d85efb62f6228b0da3ab
梁辰有点菜 <36767143+c1318821837@users.noreply.github.com>
2023-04-22 21:44:58 +0800

    Update company_repurchases.py

--- company_repurchases.py
+++ company_repurchases.py
@@ -28,6 +28,24 @@ diver.get('https://data.eastmoney.com/notices/hsa/6.html')
 print("opendiver")
 today=datetime.datetime.now().strftime('%Y-%m-%d')
 todayjson=[]
+#元素定位到回购事件
+element=diver.find_element(By.XPATH,'/html/body/div[1]/div[8]/div[2]/div[3]/div[2]/ul/li[14]')
+element.click()
+time.sleep(2)
+address='/html/body/div[1]/div[8]/div[2]/div[3]/div[3]/div[2]/div[2]/table/tbody/tr['
+for i in range(1,50):
+    elements=diver.find_element(By.XPATH,address+str(i)+']')
+    if today==elements.find_element(By.XPATH, address + str(i) + ']' + '/td[6]').text:
+        data_dict = {
+            'StockCode': elements.find_element(By.XPATH,address+str(i)+']'+'/td[1]').text,
+            'StockName':elements.find_element(By.XPATH,address+str(i)+']'+'/td[2]').text,
+            'Title':elements.find_element(By.XPATH, address + str(i) + ']' + '/td[4]').text,
+            'Time':today,
+            'ContextUrl':elements.find_element(By.XPATH, address + str(i) + ']' + '/td[4]').find_element(By.CSS_SELECTOR,'a').get_attribute('href')
+        }
+        print(data_dict)
+        todayjson.append(data_dict)
+JsonStore(JsonUpdate(JsonOpen('repurchases.json'),todayjson))
 
 diver.close()
 
