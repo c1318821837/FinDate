140d2ce598da0b471e89ebbbc9008e00884605d1
梁辰有点菜 <36767143+c1318821837@users.noreply.github.com>
2023-04-22 21:08:47 +0800

    Update company_repurchases.py

--- company_repurchases.py
+++ company_repurchases.py
@@ -5,6 +5,24 @@ import time
 import json
 from UpdateJson import JsonOpen,JsonUpdate,JsonStore
 import datetime
+
+
+def JsonOpen(path):
+    with open(path, 'r', encoding='UTF-8') as fr:
+        data = json.loads(fr.read())
+    return data
+
+def JsonUpdate(data,newdata):
+    data.append(newdata)
+    return data
+
+def JsonStore(data):
+    data= json.dumps(data, ensure_ascii=False)
+    with open('repurchases.json', 'w', encoding='UTF-8') as f:
+        f.write(data)
+        
+        
+        
 diver=webdriver.Firefox()
 diver.get('https://data.eastmoney.com/notices/hsa/6.html')
 today=datetime.datetime.now().strftime('%Y-%m-%d')
